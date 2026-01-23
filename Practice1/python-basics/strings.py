f9ccdd41ff5226bb65672c8dd9ec36da893302d8
aldy228 <al_nurbekov@kbtu.kz>
2026-01-23 11:42:18 +0500

    Add one more string

--- Practice1/python-basics/strings.py
+++ Practice1/python-basics/strings.py
@@ -2,3 +2,5 @@ age = 36
 #This will produce an error:
 txt = "My name is John, I am " + age
 print(txt)
+cat = "Kitty"
+print("Your cat's name is:" + cat) #Added one more string "cat" and print it as logical text which shows the name of a cat
