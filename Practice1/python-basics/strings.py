f4201554990b521eea154eaea05456b4cd16715c
aldy228 <al_nurbekov@kbtu.kz>
2026-01-23 11:47:53 +0500

    Add a way to avoid an error

--- Practice1/python-basics/strings.py
+++ Practice1/python-basics/strings.py
@@ -3,4 +3,4 @@ age = str(36) #Use str to avoid the error
 txt = "My name is John, I am " + age
 print(txt)
 cat = "Kitty"
-print("Your cat's name is:" + cat) #Added one more string "cat" and print it as logical text which shows the name of a cat
+print("Your cat's name is:", cat) #Added one more string "cat" and print it as logical text which shows the name of a cat
