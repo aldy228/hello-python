1d87f285ce37abbf2ce1037856452237d222b112
aldy228 <al_nurbekov@kbtu.kz>
2026-02-05 20:38:18 +0500

    Add for_continue.py with examples of for loop and continue statement usage

---
+++ Practice 2/loops/for_continue.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue #with the continue statement we can stop the current iteration, and continue with the next
+  print(x)
