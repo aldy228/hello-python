b26a5d8de9e90e572e2bfa16e6b58a374cb6cf59
aldy228 <al_nurbekov@kbtu.kz>
2026-02-05 20:36:36 +0500

    Add for_loop.py with examples of for loop usage

---
+++ Practice 2/loops/for_loop.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits: #With the for loop we can iterate over a list of items
+  print(x)
+
+for x in "banana":
+  print(x) #We can also loop through a string
+
+for x in range(6):
+  print(x) #Using the range() function to loop through a set of code a specified number of times
