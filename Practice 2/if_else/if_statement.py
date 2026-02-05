318cc893898a56f9756df55118ea55ea5cb44b65
aldy228 <al_nurbekov@kbtu.kz>
2026-02-05 20:27:56 +0500

    Add if_statement.py with examples of conditional statements and boolean usage

---
+++ Practice 2/if_else/if_statement.py
@@ -0,0 +1,23 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a") # True
+
+number = 15
+if number > 0: #checking if number is positive
+  print("The number is positive")
+
+a = 33
+b = 200
+if b > a: #idententation error example
+print("b is greater than a") # you will get an error
+
+age = 20
+if age >= 18: #multiple statements in if block
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+is_logged_in = True
+if is_logged_in: #using boolean variable in if statement
+  print("Welcome back!")
